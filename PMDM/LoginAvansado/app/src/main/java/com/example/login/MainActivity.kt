633403a3decd776d68c9ac2bca4d2d315ec78900
Hajar nassit <hajar.nassit@elorrieta-errekamari.com>
2026-09-21 20:24:42 +0200

    ok

--- PMDM/LoginAvansado/app/src/main/java/com/example/login/MainActivity.kt
+++ PMDM/LoginAvansado/app/src/main/java/com/example/login/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.login
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding: ActivityMainBinding
+    lateinit var username : EditText
+    lateinit var password : EditText
+    lateinit var button_Login : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener(View.OnClickListener{
+        if (binding.username.text.toString()=="admin" &&binding.password.text.toString() == "admin")
+        {
+            Toast.makeText(this, "Login succeful", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "Login failed!!", Toast.LENGTH_SHORT).show()
         }
+
+        })
     }
 }
